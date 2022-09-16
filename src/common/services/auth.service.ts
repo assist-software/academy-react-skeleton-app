7583582715d0870cb5f3d0502a1c3f6a26ebe4b8
Stefan Tcaciuc <stefan.tcaciuc@assist.ro>
2022-09-16 10:57:47 +0300

    Overrides getAuthUserData logic

--- src/common/services/auth.service.ts
+++ src/common/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Amplify } from 'aws-amplify'
 
-import { AuthUserData, CognitoUserAttribute, CognitoUserData } from 'common/types/auth.types'
+import { AuthUserData /*, CognitoUserAttribute, CognitoUserData*/ } from 'common/types/auth.types'
 import { Roles } from 'router/constants/roles.const'
 
 export const configureAmplify = () => {
@@ -38,38 +38,48 @@ export const getAuthIdToken = (): string => {
 }
 
 export const getAuthUserData = (): AuthUserData | null => {
-  const { REACT_APP_AWS_COGNITO_USER_POOL_WEB_CLIENT_ID: userPoolWebClientId } = process.env
-  const localStorageKeyPrefix = `CognitoIdentityServiceProvider.${userPoolWebClientId}`
-
-  const lastAuthUser = window.localStorage.getItem(`${localStorageKeyPrefix}.LastAuthUser`)
-
-  if (!lastAuthUser) {
-    return null
-  }
-
-  const userData = window.localStorage.getItem(`${localStorageKeyPrefix}.${lastAuthUser}.userData`)
-
-  if (!userData) {
-    return null
-  }
-
-  const userAttributes: CognitoUserAttribute[] = JSON.parse(userData).UserAttributes
-
-  const userAttributesObject: CognitoUserData = userAttributes.reduce(
-    (userAttributesObject: any, { Name, Value }) => {
-      userAttributesObject[Name] = Value
-      return userAttributesObject
-    },
-    {},
-  )
-
   return {
-    username: lastAuthUser,
-    name: userAttributesObject.name,
-    email: userAttributesObject.email,
-    company: userAttributesObject['custom:company'],
-    role: userAttributesObject['custom:title'],
-    industry: userAttributesObject['custom:industry'],
-    phone: userAttributesObject.phone_number,
+    username: 'cont.test.assist.ro',
+    name: 'Cont Test',
+    email: 'cont.test@assist.ro',
+    company: 'Assist',
+    role: 'SDE',
+    industry: 'IT',
+    phone: '+40753535353',
   }
+
+  // const { REACT_APP_AWS_COGNITO_USER_POOL_WEB_CLIENT_ID: userPoolWebClientId } = process.env
+  // const localStorageKeyPrefix = `CognitoIdentityServiceProvider.${userPoolWebClientId}`
+
+  // const lastAuthUser = window.localStorage.getItem(`${localStorageKeyPrefix}.LastAuthUser`)
+
+  // if (!lastAuthUser) {
+  //   return null
+  // }
+
+  // const userData = window.localStorage.getItem(`${localStorageKeyPrefix}.${lastAuthUser}.userData`)
+
+  // if (!userData) {
+  //   return null
+  // }
+
+  // const userAttributes: CognitoUserAttribute[] = JSON.parse(userData).UserAttributes
+
+  // const userAttributesObject: CognitoUserData = userAttributes.reduce(
+  //   (userAttributesObject: any, { Name, Value }) => {
+  //     userAttributesObject[Name] = Value
+  //     return userAttributesObject
+  //   },
+  //   {},
+  // )
+
+  // return {
+  //   username: lastAuthUser,
+  //   name: userAttributesObject.name,
+  //   email: userAttributesObject.email,
+  //   company: userAttributesObject['custom:company'],
+  //   role: userAttributesObject['custom:title'],
+  //   industry: userAttributesObject['custom:industry'],
+  //   phone: userAttributesObject.phone_number,
+  // }
 }
